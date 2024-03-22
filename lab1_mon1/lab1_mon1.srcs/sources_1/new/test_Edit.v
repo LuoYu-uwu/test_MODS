21eb633bc3c2a07ffc3f209b138a960d6bd7548a
liuy1103 <e0968020@u.nus.edu>
2024-03-22 09:00:30 +0800

    test Edit

---
+++ lab1_mon1/lab1_mon1.srcs/sources_1/new/test_Edit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.03.2024 08:59:24
+// Design Name: 
+// Module Name: test_Edit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test_Edit(
+
+    );
+endmodule
